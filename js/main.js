01eb0ea8a2e8268519b5bcb163eac96df8ca35ad
s1086439 <s1086439leiden@gmail.com>
2016-12-15 12:57:51 +0100

    Files

--- js/main.js
+++ js/main.js
@@ -14,10 +14,14 @@ function update(){
 	rotatieGebruiker = $("#camera").attr("rotation");
 	positieHand = {x: positieGebruiker.x, y: positieGebruiker.y - 5, z: positieGebruiker.z - 9};
 	rotatieHand = {x: rotatieGebruiker.x, y: rotatieGebruiker.y, z: rotatieGebruiker.z};
+
+	$("a-obj-model").hover( function() {
+		});
 }
 
 function dragenObject(object){
-	//$(object).attr({"position": ''+positieHand.x+' '+positieHand.y+' '+positieHand.z+'', "rotation": ''+rotatieHand.x+' '+rotatieHand.y+' '+rotatieHand.z+''});
+	$(object).attr("position", "0 -2 -6");
+	camera.add(object);
 }
 
 function draaienObject(graden, object, ms){
@@ -38,12 +42,16 @@ function fadeObject(doorzichtigheid, object, ms){
 	});
 }
 
+//$(object).attr({"position": ''+positieHand.x+' '+positieHand.y+' '+positieHand.z+'', "rotation": ''+rotatieHand.x+' '+rotatieHand.y+' '+rotatieHand.z+''});
+
 function input(){
-	$("a-obj-model").click(function(){
-		if($(this).attr('id') != "bed"){
+	$(".bed-object").click(function(){
+		alert("ok");
+		$("#object-title").attr({"bmfont-text": 'text:'+$(this).attr("id")+' fnt: ./fonts/DejaVu-sdf.fnt; fntImage: ./fonts/DejaVu-sdf.png'});
+		if($(this).attr("id") != "bed"){
 			$("#cursor").attr("material", "color: #00e203; shader: flat");
-			var draagUpdate = setInterval(dragenObject, 10, this);
-			$("#camera").append(this);
+			dragenObject(this);
+			//var draagUpdate = setInterval(dragenObject, 10, this);
 		}
 	});
 }
